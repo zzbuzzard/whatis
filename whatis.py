0ec834352eebdeb5c731a29b6a145923e9e8c721
zzb <40760215+zzbuzzard@users.noreply.github.com>
2023-12-08 21:14:51 +0000

    Initial commit

---
+++ whatis.py
@@ -0,0 +1,27 @@
+# utils for everything
+
+# what ees this object
+def whatis(obj, recurse_if_length_less_than=8, spacing=1):
+    printable=[int, float]
+    
+    pad=" "*(spacing-1)
+    if type(obj) in printable:
+        print(pad, obj)
+    else:
+        print(pad, type(obj))
+
+    try:
+        print(pad, "Shape:", obj.shape)
+    except:
+        try:
+            print(pad, "Length:", len(obj))
+        except:
+            pass
+
+    if type(obj) == tuple or type(obj) == list:
+        if len(obj) < recurse_if_length_less_than:
+            print()
+            for ind, i in enumerate(obj):
+                print(pad+" ","Index",ind)
+                whatis(i, recurse_if_length_less_than=recurse_if_length_less_than, spacing=spacing+1)
+                print()
